79b86681fa7215368a5fa974759b7b32d4c4cad5
Ömür Özkir <oemuer.oezkir@gmail.com>
2018-12-04 08:53:47 +0100

    ADD instruction sets the flags correctly

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -38,7 +38,10 @@ impl CPU {
 
     fn add(&mut self, value: u8) -> u8 {
         let (new_value, did_overflow) = self.registers.a.overflowing_add(value);
-        // TODO set flags
+        self.registers.f.zero = new_value == 0;
+        self.registers.f.substract = false;
+        self.registers.f.carry = did_overflow;
+        self.registers.f.half_carry = (self.registers.a & 0xF) + (value & 0xF) > 0xF;
         new_value
     }
 }
@@ -50,29 +53,62 @@ mod tests {
 
     #[test]
     fn add_c_to_a() {
-        let regs = Registers {
-            a: 0,
-            c: 12,
-            ..Default::default()
+        let mut cpu = CPU {
+            registers: Registers {
+                a: 0,
+                c: 12,
+                ..Default::default()
+            },
         };
-        let mut cpu = CPU { registers: regs };
         cpu.execute(Instruction::ADD(ArithmeticTarget::C));
         assert_eq!(cpu.registers.a, 12);
     }
 
     #[test]
     fn add_sets_the_flags_to_false() {
-        let regs = Registers {
-            a: 0,
-            c: 12,
-            ..Default::default()
+        let mut cpu = CPU {
+            registers: Registers {
+                a: 0,
+                c: 12,
+                ..Default::default()
+            },
         };
-        let mut cpu = CPU { registers: regs };
         cpu.execute(Instruction::ADD(ArithmeticTarget::C));
 
         assert_eq!(cpu.registers.f.zero, false);
         assert_eq!(cpu.registers.f.substract, false);
         assert_eq!(cpu.registers.f.half_carry, false);
         assert_eq!(cpu.registers.f.carry, false);
+    }
+
+    #[test]
+    fn add_with_nibble_overflow_sets_half_carry() {
+        let mut cpu = CPU {
+            registers: Registers {
+                a: 0b00000001,
+                c: 0b00001111,
+                ..Default::default()
+            },
+        };
+        cpu.execute(Instruction::ADD(ArithmeticTarget::C));
+
+        assert_eq!(cpu.registers.f.zero, false);
+        assert_eq!(cpu.registers.f.substract, false);
+        assert_eq!(cpu.registers.f.half_carry, true);
+        assert_eq!(cpu.registers.f.carry, false);
+    }
+
+    #[test]
+    fn add_with_overflow_sets_carry() {
+        let mut cpu = CPU {
+            registers: Registers {
+                a: 0b11111111,
+                c: 0b00000001,
+                ..Default::default()
+            },
+        };
+        cpu.execute(Instruction::ADD(ArithmeticTarget::C));
+
+        assert_eq!(cpu.registers.f.carry, true);
     }
 }
